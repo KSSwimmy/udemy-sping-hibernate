7df7310d7dc8b60619a38a33fb2c9b46ca8d8e89
Lambda_School_Loaner_163 <lambda_school_loaner_163@Lambda-School-Loaner-163s-MacBook-Pro.local>
2019-09-02 16:12:34 -0400

    :tada: initial commit
    initial commit

---
+++ eclipse-workspace/spring-demo-one/src/com/luv2code/springdemo/BaseballCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class BaseballCoach 
+
+{
+	public String getDailyWorkout()
+	{
+		return "Spend 30 minutes on batting paractice";
+	}
+}
+
---
+++ eclipse-workspace/spring-demo-one/src/com/luv2code/springdemo/Main.java
@@ -0,0 +1,15 @@
+package com.luv2code.springdemo;
+
+public class Main {
+
+	public static void main(String[] args) 
+	
+	{
+		// create the object
+		BaseballCoach theCoach = new BaseballCoach();
+		
+		// use the object
+		System.out.println(theCoach.getDailyWorkout());
+	}
+
+}
